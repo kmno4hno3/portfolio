1fefa94bd4e4de8f26db92805e12f512693929ec
遠藤達也 <kmno4hno3@ezweb.ne.jp>
2020-10-17 14:22:55 +0900

    introduction_done

--- front-page.php
+++ front-page.php
@@ -3,17 +3,61 @@
   <main>
 
     <!-- sec -->
-    <section class="sec section-1">
+    <section class="sec--topbg">
+        <div class="top">
+          <div class="top__inner">
+            <h1 class="top__title">Web Design</h1>
+            <p class="top__text">ご要望通りの満足頂けるオリジナルのWEBサイト制作を承ります</p>
+            <button class="btn"><a href="#">CONTACT</a></button>
+          </div>
+        </div>
     </section><!-- /sec -->
 
     <!-- sec -->
-    <section class="sec section-2">
-
+    <section class="sec">
+      <div class="introduction">
+        <div class="introduction__inner">
+          <div class="subheading">
+            <h2 class="subheading__title">Introduction</h2>
+            <p class="subheading__bar"></p>
+          </div>
+          <div class="container">
+            <p class="container__text">ダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキストダミーテキスト
+            </p>
+          </div>
+        </div>
+      </div>
     </section><!-- /sec -->
 
     <!-- sec -->
-    <section class="sec section-3">
-
+    <section class="sec">
+      <div class="service">
+        <div class="service__inner">
+          <div class="subheading">
+            <h2 class="subheading__title">SERVICE</h2>
+            <p class="subheading__bar"></p>
+          </div>
+          <div class="container">
+            <div class="col3">
+              <div class="col-item">
+                <img src="<?php echo get_template_directory_uri();?>/img/icon-pen.png" alt="">
+                <p class="">企画</p>
+                <p>ご要望を丁寧にヒアリングします。求めるものをしっかり聞き取り、お客様のご要望や目的に合わせて最もベストなプランを企画します。必ず気に入って下さるように入念に準備をしてwebサイトやバナー制作がより良いものになるように致します。</p>
+              </div>
+              <div class="col-item">
+                <img src="<?php echo get_template_directory_uri();?>/img/icon-palette.png" alt="">
+                <p>デザイン</p>
+                <p>お客様からヒアリングして練ったプランを基にデザインして行きます。目的に合わせて見た目と機能性が両立するデザインを心掛け、世界で一つだけのオリジナリティあるwebサイトを作成します。</p>
+              </div>
+              <div class="col-item">
+                <img src="<?php echo get_template_directory_uri();?>/img/icon-code.png" alt="">
+                <p>コーディング</p>
+                <p>デザインを基にアニメーションや使いやすさを追求し、これらをコーディングにより実現します。見た目だけでなくサイトを訪れた人が利用しやすいよう作り込み、集客効果を高めます。</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </section><!-- /sec -->
 
     <!-- sec -->
@@ -43,6 +87,7 @@
 
 
   </main>
+  <?php wp_footer();?>
 </body>
 
 </html>
--- functions.php
+++ functions.php
@@ -1,17 +1,39 @@
 <?php
 
-// CSSの読み込み
+// cssディレクトリのstyle.cssの読み込み
 function register_stylesheet() { // 読み込むCSSを登録する
 	wp_register_style('style', get_template_directory_uri().'/css/style.css');
 }
  
+// cssの読み込み順番を定義
 function add_stylesheet() { // 登録したCSSを以下の順番で読み込む
 	register_stylesheet();
 	wp_enqueue_style('style', '', array(), '1.0', false); // ページの末尾で読み込ませるならtrue, head内ならfalse(デフォルトはfalse)
 }
- 
 add_action('wp_enqueue_scripts', 'add_stylesheet');
 
-// ナビゲーションメニュー有効か
-register_nav_menu('header-nav', 'ヘッダーナビゲーション');
-register_nav_menu('footer-nav', 'フッターナビゲーション');
+// ナビメニューのliにclassを追加
+function add_additional_class_on_li($classes, $item, $args){
+  if(isset($args->add_li_class)){
+    $classes[] = $args->add_li_class;
+  }
+  return $classes;
+}
+add_filter('nav_menu_css_class', 'add_additional_class_on_li', 1,3);
+
+// jsの読み込み
+function my_scripts(){
+  wp_enqueue_style('my-drawer-style', 'https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/css/drawer.min.css', array(), '3.2.2', false);  
+  wp_enqueue_script('my-scroll-js', 'https://cdnjs.cloudflare.com/ajax/libs/iScroll/5.2.0/iscroll.min.js', array('jquery'), '1.0.0', true);
+  wp_enqueue_script('my-drawer-js', 'https://cdnjs.cloudflare.com/ajax/libs/drawer/3.2.2/js/drawer.min.js', array('jquery'), '1.0.0', true);
+  wp_enqueue_script('custom_script', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'my_scripts');
+
+
+// ナビゲーションメニュー設定
+function my_menu_init(){
+  register_nav_menu('header-nav', 'ヘッダーナビゲーション');
+  register_nav_menu('footer-nav', 'フッターナビゲーション');
+}
+add_action('init', 'my_menu_init');
