fcf6d8b704b4f66f1b214f6c9aa7937cd05a88a4
遠藤達也 <kmno4hno3@ezweb.ne.jp>
2020-11-06 22:23:07 +0900

    service完成

--- page-service.php
+++ page-service.php
@@ -1,40 +1,57 @@
-<?php get_header();?>
-  
+<?php get_header(); ?>
+
 <!-- sec -->
-<section class="sec">
-    <div class="inner">
-      <div class="subheading">
-        <h2 class="subheading__title">SERVICE</h2>
-        <p class="subheading__bar"></p>
+<section class="sec sec--mt">
+  <div class="inner">
+    <div class="subheading">
+      <h2 class="subheading__title">SERVICE</h2>
+      <p class="subheading__bar"></p>
+    </div>
+    <div class="container">
+      <div class="col3">
+        <div class="colItem">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/icon-pen.png" alt="" class="colItem__img">
+          <p class="colItem__tit">企画</p>
+          <p class="colItem__text">ご要望を丁寧にヒアリングします。求めるものをしっかり聞き取り、お客様のご要望や目的に合わせて最もベストなプランを企画します。必ず気に入って下さるように入念に準備をしてwebサイトやバナー制作がより良いものになるように致します。</p>
+        </div>
+        <div class="colItem">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/icon-palette.png" alt="" class="colItem__img">
+          <p class="colItem__tit">デザイン</p>
+          <p class="colItem__text">お客様からヒアリングして練ったプランを基にデザインして行きます。目的に合わせて見た目と機能性が両立するデザインを心掛け、世界で一つだけのオリジナリティあるwebサイトを作成します。</p>
+        </div>
+        <div class="colItem">
+          <img src="<?php echo get_template_directory_uri(); ?>/img/icon-code.png" alt="" class="colItem__img">
+          <p class="colItem__tit">コーディング</p>
+          <p class="colItem__text">デザインを基にアニメーションや使いやすさを追求し、これらをコーディングにより実現します。見た目だけでなくサイトを訪れた人が利用しやすいよう作り込み、集客効果を高めます。</p>
+        </div>
       </div>
-      <div class="container">
-        <div class="col3">
-          <div class="colItem">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/icon-pen.png" alt="" class="colItem__img">
-            <p class="colItem__tit">企画</p>
-            <p class="colItem__text">ご要望を丁寧にヒアリングします。求めるものをしっかり聞き取り、お客様のご要望や目的に合わせて最もベストなプランを企画します。必ず気に入って下さるように入念に準備をしてwebサイトやバナー制作がより良いものになるように致します。</p>
-          </div>
-          <div class="colItem">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/icon-palette.png" alt="" class="colItem__img">
-            <p class="colItem__tit">デザイン</p>
-            <p class="colItem__text">お客様からヒアリングして練ったプランを基にデザインして行きます。目的に合わせて見た目と機能性が両立するデザインを心掛け、世界で一つだけのオリジナリティあるwebサイトを作成します。</p>
-          </div>
-          <div class="colItem">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/icon-code.png" alt="" class="colItem__img">
-            <p class="colItem__tit">コーディング</p>
-            <p class="colItem__text">デザインを基にアニメーションや使いやすさを追求し、これらをコーディングにより実現します。見た目だけでなくサイトを訪れた人が利用しやすいよう作り込み、集客効果を高めます。</p>
-          </div>
-        </div>
-        <div class="detail">
-          <p class="detail__txt">
-            <a href="#" class="detail__link">サービスの詳細
-              <i class="detail__icon fa fa-chevron-right"></i>
-            </a>
-          </p>
-        </div>
+      <div class="detail">
+        <p class="detail__txt">
+          <a href="#" class="detail__link">サービスの詳細
+            <i class="detail__icon fa fa-chevron-right"></i>
+          </a>
+        </p>
       </div>
     </div>
-  </section><!-- /sec -->
+  </div>
+</section><!-- /sec -->
+
+<section class="sec">
+  <div class="inner">
+    <div class="subheading">
+      <h2 class="subheading__title">Details</h2>
+      <p class="subheading__bar"></p>
+    </div>
+    <div class="container">
+      <div class="menu">
+        <li class="menu__item">Wordpressによるお客様でも管理がしやすいウェブサイトの構築。</li>
+        <li class="menu__item">集客が見込まれるようにSEO対策の実施。</li>
+        <li class="menu__item">Photoshopによる画像の加工。</li>
+        <li class="menu__item">ウェブサイトの改修と保守。</li>
+      </div>
+    </div>
+  </div>
+</section>
 
 
-<?php get_footer();?>
+<?php get_footer(); ?>
