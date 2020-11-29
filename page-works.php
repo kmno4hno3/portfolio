2df0f03c65b5b2886e0bbf7b3b7cd3e91830265b
遠藤達也 <kmno4hno3@ezweb.ne.jp>
2020-11-29 12:03:02 +0900

    work完成

--- page-works.php
+++ page-works.php
@@ -7,6 +7,7 @@
       <p class="subheading__bar"></p>
     </div>
     <div class="container">
+    <div class="col1">
       <?php
         $args = array(
           'posts_per_page',
@@ -17,17 +18,32 @@
         $my_posts = get_posts($args);
       ?>
       <?php foreach($my_posts as $post): setup_postdata($post);?>
-      <a href="<?php echo get_permalink($post->ID)?>">
+      <div class="item" href="<?php echo get_permalink($post->ID)?>">
+        <p class="item__tit"><?php echo my_the_title(get_the_title($post->ID)); ?></p>
         <?php
           $thumbnail_id = get_post_thumbnail_id($post->ID);
           $thumb_url = wp_get_attachment_image_src($thumbnail_id, 'small');
 
           if(get_post_thumbnail_id($post->ID)){
-            echo '<figure><img src="' . $thumb_url[0] . '"></figure>';
+            echo '<figure class=""><img src="' . $thumb_url[0] . '"></figure>';
           }
+
+          $skill_field =  get_field('skill', $post->ID);
+          echo '<p class="item__skill">' . $skill_field . '</p>'; // スキル
+
+          $work_info_field =  get_field('work_info', $post->ID);
+          echo '<p class="item__workinfo">' . $work_info_field . '</p>'; // work情報
+
+          $site_url_field =  get_field('site_url', $post->ID);
+          $repalced_url = preg_replace('#^https?://#', '', $site_url_field);
+          echo '<a class="item__siteurl" href="' . $site_url_field . '">' . $repalced_url . '</a>'; // サイトURL
+
+
+
         ?>
-      </a>
+      </div>
       <?php endforeach;?>
+      </div>
     </div>
   </div>
 </section>
