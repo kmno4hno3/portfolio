3613a4564412c6b35813e31c4cbb5944face41d9
遠藤達也 <kmno4hno3@ezweb.ne.jp>
2020-12-06 08:06:47 +0900

    カテゴリーページ完成

--- category.php
+++ category.php
@@ -1,7 +1,24 @@
 <?php get_header(); ?>
 
-<main>
-
-</main>
+<section class="sec sec--mt">
+  <div class="inner">
+    <div class="subheading">
+      <h2 class="subheading__title">BLOG</h2>
+      <p class="subheading__bar"></p>
+    </div>
+    <div class="container">
+      <div class="col2 col2--blog">
+        <div class="main">
+          <div class="col1 col1--blog">
+            <?php
+              get_template_part('loop-content');
+            ?>
+          </div>
+        </div>
+        <?php get_sidebar();?>
+      </div>
+    </div>
+  </div>
+</section>
 
 <?php get_footer(); ?>
